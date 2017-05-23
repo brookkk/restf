0730c1372d9f03b6325c0f65cda1e37fa543a51e
brook <grini.abdellah@gmail.com>
2017-05-23 09:33:11 +0100

    restf00

---
+++ src/Rest/PlatformBundle/Entity/Author.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Rest\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Author
+ *
+ * @ORM\Table(name="author")
+ * @ORM\Entity(repositoryClass="Rest\PlatformBundle\Repository\AuthorRepository")
+ */
+class Author
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fullname", type="string", length=255)
+     */
+    private $fullname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="biography", type="string", length=255)
+     */
+    private $biography;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fullname
+     *
+     * @param string $fullname
+     *
+     * @return Author
+     */
+    public function setFullname($fullname)
+    {
+        $this->fullname = $fullname;
+
+        return $this;
+    }
+
+    /**
+     * Get fullname
+     *
+     * @return string
+     */
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * Set biography
+     *
+     * @param string $biography
+     *
+     * @return Author
+     */
+    public function setBiography($biography)
+    {
+        $this->biography = $biography;
+
+        return $this;
+    }
+
+    /**
+     * Get biography
+     *
+     * @return string
+     */
+    public function getBiography()
+    {
+        return $this->biography;
+    }
+}
+
---
+++ src/Rest/PlatformBundle/RestPlatformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rest\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RestPlatformBundle extends Bundle
+{
+}
