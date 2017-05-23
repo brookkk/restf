4b864e422f69c4a3c2c26c175e4b89ebf20a00d7
brook <grini.abdellah@gmail.com>
2017-05-23 12:47:28 +0100

    restf03

--- src/Rest/PlatformBundle/Controller/ArticleAPIController.php
+++ src/Rest/PlatformBundle/Controller/ArticleAPIController.php
@@ -2,6 +2,16 @@
 
 namespace Rest\PlatformBundle\Controller;
 
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
+
+use Rest\PlatformBundle\Entity\Article;
+use Rest\PlatformBundle\Entity\Author;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArticleAPIController extends Controller
@@ -10,4 +20,59 @@ class ArticleAPIController extends Controller
     {
         return $this->render('RestPlatformBundle:Default:index.html.twig');
     }
+
+
+
+
+    /**
+     * @Get(
+     *     path = "/articles/{id}",
+     *     name = "article_show",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @View
+     */
+    public function articleShowAction(Article $article)
+    {
+    	return $article;
+    }
+
+
+
+
+
+
+
+     /**
+  * @Rest\Post(
+     *    path = "/articles",
+     *    name = "app_article_create"
+     * )
+    * @Rest\View(StatusCode = 201)
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+    public function articleCreateAction(Article $article)
+    {
+         $em = $this->getDoctrine()->getManager();
+
+        $em->persist($article);
+        $em->flush();
+
+        return $article;
+    }
+    
+
+
+  /**
+     * @Rest\Get("/articles", name="article_list")
+     */
+    public function listAction()
+    {
+        $articles = $this->getDoctrine()->getRepository('RestPlatformBundle:Article')->findAll();
+        
+        return $articles;
+    }
+
+
+
 }
